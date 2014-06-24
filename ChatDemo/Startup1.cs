5c88b996a312f7a82599bad4753adac8ccbe54b2
clintonmedbery <clintomed@gmail.com>
2014-06-24 12:13:09 -0400

    ChatDemo
    Chat Room demo using ASP.NET and SignalR

---
+++ ChatDemo/Startup1.cs
@@ -0,0 +1,18 @@
+﻿
+
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(ChatDemo.Startup1))]
+
+namespace ChatDemo
+{
+    public class Startup1
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=316888
+            app.MapSignalR();
+        }
+    }
+}
